205949f02f127e574b005bbf9adb8bc81d8db507
JM <jose.copino@vcdcph.com>
2022-07-13 15:03:15 +0800

    feature/HRIS-82: modify readme

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import configuration from 'config/configuration';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const {port} = configuration()
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -18,6 +21,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('/swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
