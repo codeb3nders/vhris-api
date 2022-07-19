e67a921abd6a530f3b7952b725240178850a88a4
JM <jose.copino@vcdcph.com>
2022-07-19 14:05:18 +0800

    feature/HRIS: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import configuration from 'config/configuration';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
 
   const {port} = configuration()
 
