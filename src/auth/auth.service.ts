3e6546a618cd8804934be4a58606b3e71226caf3
JM <jose.copino@vcdcph.com>
2022-08-17 17:10:25 +0800

    feature/VHRIS: add user info

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,13 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { EmployeesService } from 'src/employees/employees.service';
 import { UserCredentialsService } from 'src/user_credentials/user_credentials.service';
 import { comparePassword } from 'src/utils/encoder';
 @Injectable()
 export class AuthService {
   constructor(
     private userCredentialService: UserCredentialsService,
+    private employeeService: EmployeesService,
     private jwtService: JwtService,
   ) {}
 
@@ -30,9 +32,10 @@ export class AuthService {
       isActive,
       employeeNo,
     };
-
+    const employee = await this.employeeService.findOne(employeeNo);
     return {
       access_token: this.jwtService.sign(payload),
+      userInfo: employee,
     };
   }
 }
